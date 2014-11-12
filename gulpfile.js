be020ade9d56bd7d1e0bf5c3da28681e465f1289
Jevgeni Krais <jevgeni@pitfire.eu>
2014-11-12 13:16:49 +0200

    added gulp and dependencies, updated .gitignore

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+var spawn = require('child_process').spawn;
+var node;
+
+gulp.task('reload', ['js'], function () {
+	if (node) node.kill()
+		node = spawn('node', ['index.js'], {stdio: 'inherit'});
+
+	node.on('close', function (code) {
+		if (code === 8) {
+			console.log('Error detected, waiting for changes...');
+		}
+	});
+});
+
+gulp.task('js', function() {
+	gulp.src([
+		'./index.js',
+		'./app/**/*.js',
+	])
+	.pipe(jshint())
+	.pipe(jshint.reporter(stylish))
+});
+
+gulp.task('watch', function () {
+	gulp.start('reload', function() {});
+
+	watch(['index.js', 'app/**/*.js'], function (files, cb) {
+		gulp.start('reload', cb);
+	});
+});
